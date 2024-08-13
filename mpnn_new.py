4d56ce1b4375597b3991fd3b7b7061af5e7d7ab8
Malika Tokpanova <malikatokpanova@gmail.com>
2024-08-13 14:33:13 +0200

    Initial commit

---
+++ mpnn_new.py
@@ -0,0 +1,337 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from itertools import combinations, chain, islice
+from itertools import product
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import Linear
+import networkx as nx
+import os
+import torch.optim as optim
+from torch.optim import Adam
+from torch.optim import SGD
+from math import ceil
+import argparse
+from matplotlib.pylab import plt
+from torch.nn import Sequential as Seq, Linear, ReLU, LeakyReLU
+from torch.nn import Linear, Sequential, ReLU, BatchNorm1d as BN
+from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau
+from torch_scatter import scatter_min, scatter_max, scatter_add, scatter_mean
+
+from torch_geometric.utils import  remove_self_loops
+from sklearn.model_selection import ParameterGrid
+import random
+from random import sample
+from new_model import ramsey_MPNN, loss_func, cost
+
+import wandb
+
+
+import argparse
+from pathlib import Path
+
+""" parser = argparse.ArgumentParser()
+
+#parser.add_argument('num_nodes', type=int, help='Number of nodes')
+parser.add_argument('--hidden_channels', type=int, help='Number of hidden channels', default=32)
+parser.add_argument('--num_features', type=int, help='Number of features', default=32)
+
+parser.add_argument('--learning_rate_1', type=float, help='Learning rate 1', default=0.001)
+parser.add_argument('--learning_rate_2', type=float, help='Learning rate 2', default=0.01)
+parser.add_argument('--epochs', type=int, help='Number of epochs', default=100)
+
+args = parser.parse_args()
+
+#num_samples = args.num_samples
+hidden_channels=args.hidden_channels
+num_features=args.num_features
+lr_1=args.learning_rate_1
+lr_2=args.learning_rate_2
+
+epochs = args.epochs """
+
+config=dict(
+        hidden_channels= 64,
+        num_features= 32,
+        lr_1=0.001,
+        lr_2=0.01,
+        seed=0,
+)
+
+""" sweep_config={
+    'method':'grid',
+    'parameters':{
+        'hidden_channels':{
+            'values': list(range(32,128))
+        },
+        'num_features':{
+            'values': list(range(32,64))
+        },
+        'lr_1':{
+            'values':[0.001,0.01,0.0001]
+        },
+        'lr_2':{
+            'values':[0.001,0.01,0.0001]
+        },
+        'seed':{
+            'values':list(range(0,10))
+        }
+    }
+}
+sweep_id=wandb.sweep(sweep_config, project='project')
+"""
+graph_parameters={
+    'num_nodes': 14,   
+    'num_cliques':256, # 256
+    'clique_r':4,
+    'clique_s':4
+}
+
+
+num_nodes=graph_parameters['num_nodes']
+num_cliques=graph_parameters['num_cliques']
+clique_r=graph_parameters['clique_r']
+clique_s=graph_parameters['clique_s']
+
+
+lr_decay_step_size = 10
+lr_decay_factor = 0.95
+
+
+epochs = 100
+retdict = {}
+""" edge_drop_p = 0.0
+edge_dropout_decay = 0.90
+ """
+
+#for plotting loss values
+train_loss_dict={}
+
+
+#train
+def train_model(net,optimizer_1,optimizer_2,num_nodes, hidden_channels,num_features, learning_rate_1,learning_rate_2, epochs, lr_decay_step_size, lr_decay_factor, clique_r, num_cliques,all_cliques_r,all_cliques_s):
+
+
+    """ net=ramsey_MPNN(num_nodes, hidden_channels,num_features)
+    #net.reset_parameters()    
+    #net.train()
+    
+    #optimizer = Adam(net.parameters(), lr=learning_rate, weight_decay=0.00000)
+    params=[param for name, param in net.named_parameters() if 'edge_pred_net' not in name]
+    optimizer_1=Adam(params, lr=learning_rate_1, weight_decay=0.0)
+    optimizer_2= Adam(net.edge_pred_net.parameters(), lr=learning_rate_2, weight_decay=0.0) """
+    
+    
+    net.train()
+    wandb.watch(net,log='all',log_freq=10)
+    
+    for epoch in range(epochs):
+        count=0
+
+        """ if epoch % 5 == 0:
+            edge_drop_p = edge_drop_p*edge_dropout_decay
+            print("Edge_dropout: ", edge_drop_p) """
+
+        """ if epoch % lr_decay_step_size == 0:
+            for param_group in optimizer_1.param_groups:
+                    param_group['lr'] = lr_decay_factor * param_group['lr']
+            for param_group in optimizer_2.param_groups:
+                    param_group['lr'] = lr_decay_factor * param_group['lr']  """
+                    
+        count += 1 
+        
+        optimizer_1.zero_grad()
+        optimizer_2.zero_grad()
+            
+        #cliques_r=torch.randint(0,num_nodes, (num_cliques, clique_r))
+        #cliques_s=torch.randint(0,num_nodes, (num_cliques, clique_s))
+        cliques_r=random.sample(list(all_cliques_r),num_cliques)
+        cliques_s=random.sample(list(all_cliques_s),2*num_cliques)
+        #cliques=torch.combinations(torch.arange(num_nodes),clique_r)
+        probs=net(torch.randn(net.num_nodes, net.num_features))
+        loss=loss_func(probs,cliques_r,cliques_s)
+        loss.backward()
+        
+        if epoch%10==0:
+            wandb.log({"epoch": epoch, "loss": loss.item()})
+            print('Epoch: ', epoch, 'loss:', loss.item())
+    
+        torch.nn.utils.clip_grad_norm_(net.parameters(),1)
+        
+        optimizer_1.step()
+        optimizer_2.step()
+        
+        train_loss_dict[epoch]=loss.item() 
+        
+        
+
+#torch.manual_seed(0)
+
+def make(config):
+    net=ramsey_MPNN(num_nodes, config.hidden_channels,config.num_features) 
+    net.reset_parameters()
+    params=[param for name, param in net.named_parameters() if 'edge_pred_net' not in name]
+    optimizer_1=Adam(params, lr=config.lr_1, weight_decay=0.0)
+    optimizer_2= Adam(net.edge_pred_net.parameters(), lr=config.lr_2, weight_decay=0.0)
+
+    all_cliques_r=torch.combinations(torch.arange(num_nodes),clique_r) 
+    all_cliques_s=torch.combinations(torch.arange(num_nodes),clique_s)
+    #train_model(net,optimizer_1,optimizer_2,num_nodes,config.hidden_channels,config.num_features,config.lr_1, config.lr_2,  config.epochs, lr_decay_step_size, lr_decay_factor, clique_r, num_cliques,all_cliques_r,all_cliques_s)#,hidden_2,edge_drop_p,edge_dropout_decay)
+    #torch.save(net.state_dict(), f'model_{num_nodes}_{config.hidden_channels}_{config.num_features}_{config.lr_1}_{config.lr_2}.pth')
+    return net, optimizer_1, optimizer_2, all_cliques_r, all_cliques_s
+
+#plotting loss
+""" 
+plt.clf()
+fig,ax=plt.subplots(figsize=(9,7))
+epochs_=range(0,epochs)
+plt.plot(epochs_,train_loss_dict.values(), label='Training loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.title('Training loss')
+
+textstr='\n'.join((
+fr'$N={num_nodes}$',
+f'$\\beta_1={lr_1}$',
+f'$\\beta_2={lr_2}$',
+f'$\\gamma={hidden_channels}$',
+fr'$\omega={num_features}$'))
+
+
+props = dict(boxstyle='square', facecolor='white', alpha=0.5)
+
+ax.text(0.82, 0.95, textstr, transform=ax.transAxes, fontsize=14,
+        verticalalignment='top', bbox=props)
+
+
+#plt.legend()
+#plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
+plt.savefig(f'loss_{num_nodes}_{hidden_channels}_{num_features}_{lr_1}_{lr_2}.png')
+plt.close    
+"""   
+def mc_sampling_new(probs, num_samples):
+    samples = torch.zeros(num_samples, *probs.shape)
+    
+    for i in range(num_samples):
+        samples[i] = torch.bernoulli(probs)
+    return samples
+
+def optimal_new(all_cliques_r,all_cliques_s,num_samples, samples): 
+    #samples=mc_sampling(probs,num_samples)
+    costs=torch.zeros(num_samples)
+    for i in range(num_samples):
+        cost_p=cost(samples[i], all_cliques_r,all_cliques_s)
+        costs.scatter_add_(0,torch.tensor([i]),cost_p)
+    min_cost, min_index = torch.min(costs,0)
+    return min_cost #samples[min_index]
+
+#retrieve deterministically
+def decode_graph(num_nodes,probs,cliques_r,cliques_s):
+    edge_index = torch.combinations(torch.arange(num_nodes), r=2).t()
+    flat_probs = probs[edge_index[0], edge_index[1]]
+    sorted_inds = torch.argsort(flat_probs, descending=True)
+    
+    sets = probs.detach().clone()  
+    
+    for flat_index in sorted_inds:
+        
+        edge = edge_index[:, flat_index]
+        src, dst = edge[0].item(), edge[1].item()
+        
+        graph_probs_0 = sets.clone()
+        graph_probs_1 = sets.clone()
+        
+        
+        graph_probs_0[src, dst] = 0
+        graph_probs_0[dst,src] = 0  
+        
+        graph_probs_1[src, dst] = 1
+        graph_probs_1[dst,src] = 1  
+        
+        
+        #expected_obj_0 = cost(graph_probs_0, cliques_r,cliques_s) #initial, edge is red
+        #expected_obj_1 = cost(graph_probs_1, cliques_r,cliques_s) #edge is blue in the solution
+        expected_obj_0 = loss_func(graph_probs_0, cliques_r,cliques_s) #initial, edge is red
+        expected_obj_1 = loss_func(graph_probs_1, cliques_r,cliques_s)
+            
+        if expected_obj_0 > expected_obj_1: 
+            sets[src, dst] = 1  # Edge is blue
+            sets[dst,src] = 1  
+        else:
+            sets[src, dst] = 0  # Edge is red
+            sets[dst,src] = 0  
+    expected_obj_G = cost(sets, cliques_r,cliques_s)
+    return sets, expected_obj_G.detach() #returning the coloring and its cost
+
+    
+def evaluate(net,cliques_r,cliques_s, hidden_channels,num_features,lr_1,lr_2):
+    results = {}
+    results_sampling={}
+    num_samples=100
+    #net=ramsey_MPNN(num_nodes, hidden_channels,num_features) 
+    with torch.no_grad():
+        
+        #net.load_state_dict(torch.load(f'model_{num_nodes}_{hidden_channels}_{num_features}_{lr_1}_{lr_2}.pth'))
+        #net.load_state_dict(torch.load(f'model_{num_nodes}_{hidden_channels}_{lr_1}_{lr_2}.pth'))
+        net.eval()
+        probs=net(torch.randn(net.num_nodes, net.num_features))
+        results_fin=decode_graph(num_nodes,probs,cliques_r,cliques_s)
+        """ coloring=mc_sampling_new(probs, num_samples)
+        results_sampling[num_nodes]=optimal_new(cliques_r,cliques_s,num_samples, coloring) """
+        
+        """  params_key = str(params)
+        if params_key not in results:
+            results[params_key] = {}
+            
+        results[params_key][num_nodes]=results_fin
+        """
+        wandb.log({"cost": results_fin[1]})#, "coloring":results_fin[0]})
+    torch.onnx.export(net, torch.randn(net.num_nodes, net.num_features), f'model_{num_nodes}_{hidden_channels}_{num_features}_{lr_1}_{lr_2}.onnx')
+    wandb.save(f'model_{num_nodes}_{hidden_channels}_{num_features}_{lr_1}_{lr_2}.onnx')
+    return results_fin
+        
+#results, sets=evaluate(num_nodes, clique_r, clique_s)
+
+def model_pipeline(hyperparameters):
+    with wandb.init(project="project", config=hyperparameters):
+        config = wandb.config
+        
+        torch.manual_seed(config.seed)
+        np.random.seed(config.seed)
+        random.seed(config.seed)
+
+        net, optimizer_1, optimizer_2, all_cliques_r, all_cliques_s = make(config)
+        train_model(net,optimizer_1,optimizer_2,num_nodes,config.hidden_channels,config.num_features,config.lr_1, config.lr_2,  epochs, lr_decay_step_size, lr_decay_factor, clique_r, num_cliques,all_cliques_r,all_cliques_s)#,hidden_2,edge_drop_p,edge_dropout_decay)
+        
+        torch.save(net.state_dict(), f'model_{num_nodes}_{config.hidden_channels}_{config.num_features}_{config.lr_1}_{config.lr_2}.pth')
+        net.load_state_dict(torch.load(f'model_{num_nodes}_{config.hidden_channels}_{config.num_features}_{config.lr_1}_{config.lr_2}.pth'))
+        
+        evaluate(net,all_cliques_r,all_cliques_s,config.hidden_channels,config.num_features,config.lr_1,config.lr_2)
+        print(evaluate(net,all_cliques_r,all_cliques_s,config.hidden_channels,config.num_features,config.lr_1,config.lr_2))
+        
+        return net
+    
+#wandb.agent(sweep_id, model_pipeline)
+net=model_pipeline(config)
+
+
+""" color_dict={0:'red', 1:'blue'}
+
+graph_coloring=nx.Graph()
+#sets[0]
+for i in range(num_nodes):
+    for j in range(num_nodes):
+        if i<j:
+            if sets[0][i,j]==1:
+                graph_coloring.add_edge(i,j, color='blue')
+            else:
+                graph_coloring.add_edge(i,j, color='red')
+            
+edge_colors=[graph_coloring[u][v]['color'] for u,v in graph_coloring.edges()]
+pos = nx.circular_layout(graph_coloring)  
+plt.title('Ramsey (3,3)-graph on 5 vertices')
+nx.draw(graph_coloring, pos, edge_color=edge_colors, with_labels=True, node_color='lightgrey', node_size=500)
+
+plt.show()   
+"""
---
+++ new_model.py
@@ -0,0 +1,163 @@
+
+from itertools import combinations, chain, islice
+import numpy as np
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import Linear
+from torch.nn import Sequential as Seq, Linear, ReLU, LeakyReLU, BatchNorm1d
+
+from torch_geometric.utils import degree
+from torch_geometric.nn import GINConv, GATConv, GCNConv, SAGEConv, GatedGraphConv
+
+from torch.nn import Sequential as Seq, Linear, ReLU, LeakyReLU
+from torch_geometric.nn import MessagePassing
+from torch.nn import Linear, Sequential, ReLU, BatchNorm1d as BN
+
+from torch_scatter import scatter_min, scatter_max, scatter_add, scatter_mean
+
+from torch_geometric.utils import softmax, add_self_loops, remove_self_loops, segregate_self_loops, remove_isolated_nodes, contains_isolated_nodes, add_remaining_self_loops, dropout_adj
+from dgl.nn.pytorch import GraphConv
+from time import time
+
+#from layers.mlp_readout_layer import MLPReadout
+                    
+class ramsey_MPNN(torch.nn.Module):
+    def __init__(self, num_nodes, hidden_channels,num_features,heads=3):#num_layers, hidden1, hidden2,mask_prob=0.1):
+        super(ramsey_MPNN, self).__init__()
+        self.num_features=num_features
+        self.num_nodes=num_nodes
+        self.hidden_channels=hidden_channels
+        #self.node_embedding = nn.Embedding(num_nodes, num_features)
+        self.node_features = torch.nn.Parameter(torch.randn(num_nodes, num_features),requires_grad=True) 
+        
+        #torch.nn.init.kaiming_normal_(self.node_features)
+        #nn.init.kaiming_uniform_(self.node_features, nonlinearity='relu')
+        #nn.init.xavier_uniform_(self.node_features) 
+        #nn.init.uniform_(self.node_features, a=0.0, b=1.0)
+        self.conv1 = GINConv(Sequential(Linear(num_features, hidden_channels),  BatchNorm1d(hidden_channels),ReLU(), Linear(hidden_channels,num_features), ReLU()))
+        self.conv2 = GINConv(Sequential(Linear(num_features, hidden_channels), BatchNorm1d(hidden_channels), ReLU(), Linear(hidden_channels,num_features), ReLU())) #changed
+        #self.conv3 = GINConv(Sequential(Linear(hidden_channels, hidden_channels), BatchNorm1d(hidden_channels), ReLU(), Linear(hidden_channels, num_features), ReLU()))
+        self.lin1=Linear(num_features,hidden_channels)
+        
+        self.lin2=Linear(hidden_channels,num_features)  
+    
+        self.edge_pred_net = EdgePredNet(num_features,hidden_channels) 
+        
+    def reset_parameters(self):
+        self.conv1.reset_parameters()
+        self.conv2.reset_parameters()
+        #self.conv3.reset_parameters()
+        
+        self.lin1.reset_parameters()
+        self.lin2.reset_parameters() 
+        #nn.init.uniform_(self.node_embedding.weight, -0.1, 0.1)
+        #torch.nn.init.xavier_uniform_(self.node_features) 
+        #torch.nn.init.kaiming_normal_(self.node_features)
+        #nn.init.kaiming_uniform_(self.node_features, nonlinearity='relu')
+        #nn.init.uniform_(self.node_features, a=0.0, b=1.0)
+        #self.edge_pred_net.reset_parameters()    
+        #nn.init.normal_(self.node_features, mean=0, std=1) 
+    
+    def forward(self,x):
+        x = self.node_features
+        
+        #x=self.node_embedding.weight
+        num_nodes = x.shape[0]
+        edge_index = torch.combinations(torch.arange(self.num_nodes), r=2).t()
+        
+        xinit=x
+        
+        x=self.conv1(x, edge_index) 
+        x=x+xinit
+        x=F.leaky_relu(x)
+        x=F.dropout(x, p=0.32, training=self.training) 
+        
+        
+        """ x=self.conv2(x, edge_index)
+        x=F.leaky_relu(x,negative_slope=0.02)
+        x=F.dropout(x, p=0.32, training=self.training)
+        x=self.conv3(x, edge_index)  """
+        #x=x+self.conv2(x, edge_index)
+    
+    
+        x=self.lin1(x)
+        x=F.leaky_relu(x)
+        x=F.dropout(x, p=0.3, training=self.training) 
+        x=self.lin2(x) 
+                  
+        probs = torch.zeros(num_nodes, num_nodes)
+        edge_pred = self.edge_pred_net(x, edge_index)
+        
+        probs[edge_index[0], edge_index[1]] = edge_pred.squeeze()
+        probs[edge_index[1], edge_index[0]] = edge_pred.squeeze() 
+        
+        
+        return probs
+    
+class EdgePredNet(torch.nn.Module):
+    def __init__(self,num_features,hidden_channels):
+        super(EdgePredNet, self).__init__() 
+        self.lin = Sequential(Linear(2*num_features, 64), ReLU(), Linear(64, 1),torch.nn.Sigmoid())
+        #self.lin = Sequential(Linear(2*num_features, hidden_channels), LeakyReLU(), Linear(hidden_channels, 1), torch.nn.Sigmoid())
+    def forward(self, x, edge_index):
+        x_i = x[edge_index[0], :]
+        x_j = x[edge_index[1], :]
+        edge_features = torch.cat([x_i, x_j], dim=-1)  
+
+        return self.lin(edge_features)
+
+def loss_func(probs, cliques_r,cliques_s):
+    loss = 0
+    
+    for clique in cliques_r:   
+        edge_indices=torch.combinations(clique, r=2).t()
+        edge_indices = edge_indices[:, edge_indices[0] < edge_indices[1]]
+        edge_probs = probs[edge_indices[0], edge_indices[1]]
+        
+        blue_prod = edge_probs.prod()
+        
+        loss += blue_prod 
+    for clique in cliques_s:
+        edge_indices=torch.combinations(clique, r=2).t()
+        edge_indices = edge_indices[:, edge_indices[0] < edge_indices[1]]
+        edge_probs = probs[edge_indices[0], edge_indices[1]]
+
+        red_prod = (1 - edge_probs).prod()
+    
+        loss += red_prod 
+    
+    if len(cliques_r[0])!=len(cliques_s[0]):
+        N=len(cliques_r)+len(cliques_s)
+    else:
+        N=len(cliques_r)
+    return loss/N 
+
+    
+#evaluation
+def cost(probs, cliques_r,cliques_s):
+    expectation = 0
+    for clique in cliques_r:   
+        edge_indices=torch.combinations(clique, r=2).t()
+        edge_indices = edge_indices[:, edge_indices[0] < edge_indices[1]]
+        edge_probs = probs[edge_indices[0], edge_indices[1]]
+        
+        blue_prod = edge_probs.prod()
+        
+        expectation += blue_prod
+    for clique in cliques_s:
+        edge_indices=torch.combinations(clique, r=2).t()
+        edge_indices = edge_indices[:, edge_indices[0] < edge_indices[1]]
+        edge_probs = probs[edge_indices[0], edge_indices[1]]
+
+        red_prod = (1 - edge_probs).prod()
+    
+        expectation += red_prod
+    
+    return expectation
+
+
+
+
