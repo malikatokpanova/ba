8020d7fcae2d4a37a311565343f25669c79c41ae
Malika Tokpanova <malikatokpanova@gmail.com>
2024-09-03 16:30:56 +0200

    updated

--- mpnn_new.py
+++ mpnn_new.py
@@ -75,7 +75,7 @@ lr_decay_step_size = 20
 lr_decay_factor = 0.95
 
 
-epochs = 6000
+epochs = 10000
 retdict = {}
 """ edge_drop_p = 0.0
 edge_dropout_decay = 0.90
@@ -93,7 +93,7 @@ def train_model(net,optimizer_1,optimizer_2,num_nodes, hidden_channels,num_featu
     
     for epoch in range(epochs):
         count=0
-        if epoch== 4000:
+        if epoch== 5000:
             net.node_features.requires_grad = False 
         """ if epoch % 5 == 0:
             edge_drop_p = edge_drop_p*edge_dropout_decay
@@ -113,7 +113,7 @@ def train_model(net,optimizer_1,optimizer_2,num_nodes, hidden_channels,num_featu
         #cliques_r=torch.randint(0,num_nodes, (num_cliques, clique_r))
         #cliques_s=torch.randint(0,num_nodes, (num_cliques, clique_s))
         cliques_r=random.sample(all_cliques_r.tolist(),num_cliques)
-        cliques_s=random.sample(all_cliques_s.tolist(),2*num_cliques)
+        cliques_s=random.sample(all_cliques_s.tolist(),3*num_cliques)
         
         cliques_r=torch.tensor(cliques_r,dtype=torch.long).to(device)
         cliques_s=torch.tensor(cliques_s,dtype=torch.long).to(device)
