32aa2eb676c82f989dc5667e43701962d8da0693
Malika Tokpanova <malikatokpanova@gmail.com>
2024-09-03 19:45:57 +0200

    node features initialization

--- mpnn_new.py
+++ mpnn_new.py
@@ -115,10 +115,10 @@ def train_model(net,optimizer_1,optimizer_2,num_nodes, hidden_channels,num_featu
         #cliques_r=random.sample(all_cliques_r.tolist(),num_cliques)
         #cliques_s=random.sample(all_cliques_s.tolist(),3*num_cliques)
         
-        #cliques_r=torch.tensor(cliques_r,dtype=torch.long).to(device)
-        #cliques_s=torch.tensor(cliques_s,dtype=torch.long).to(device)
-        cliques_r=all_cliques_r
-        cliques_s=all_cliques_s
+        cliques_r=torch.tensor(cliques_r,dtype=torch.long).to(device)
+        cliques_s=torch.tensor(cliques_s,dtype=torch.long).to(device)
+        #cliques_r=all_cliques_r
+        #cliques_s=all_cliques_s
         #cliques=torch.combinations(torch.arange(num_nodes),clique_r)
         probs=net(torch.randn(net.num_nodes, net.num_features).to(device))
         loss=loss_func(probs,cliques_r,cliques_s)
