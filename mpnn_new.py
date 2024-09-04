f2d4201559da6cab3ed917b59aba6cb1b882073a
Malika Tokpanova <malikatokpanova@gmail.com>
2024-09-04 20:55:46 +0200

    updated

--- mpnn_new.py
+++ mpnn_new.py
@@ -113,7 +113,7 @@ def train_model(net,optimizer_1,optimizer_2,num_nodes, hidden_channels,num_featu
         #cliques_r=torch.randint(0,num_nodes, (num_cliques, clique_r))
         #cliques_s=torch.randint(0,num_nodes, (num_cliques, clique_s))
         cliques_r=random.sample(all_cliques_r.tolist(),num_cliques)
-        cliques_s=random.sample(all_cliques_s.tolist(),2*num_cliques)
+        cliques_s=random.sample(all_cliques_s.tolist(),num_cliques)
         
         cliques_r=torch.tensor(cliques_r,dtype=torch.long).to(device)
         cliques_s=torch.tensor(cliques_s,dtype=torch.long).to(device)
