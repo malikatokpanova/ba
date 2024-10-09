70e99f25e9514ea91dd5a0305b938334ffd8d311
Malika Tokpanova <malikatokpanova@gmail.com>
2024-10-09 14:17:50 +0200

    baseline

--- baseline.py
+++ baseline.py
@@ -21,22 +21,6 @@ torch.backends.cudnn.benchmark = False
 torch.manual_seed(0)
 random.seed(0)
 
-class Net(nn.Module):
-    def __init__(self,input_dim,hidden_dim,num_classes):
-        super(Net, self).__init__()
-        self.fc1 = Linear(input_dim, hidden_dim)
-        self.fc2 = Linear(hidden_dim, hidden_dim)
-        self.fc3=Linear(hidden_dim,hidden_dim)
-        self.fc4=Linear(hidden_dim,hidden_dim)
-        self.fc5=Linear(hidden_dim,num_classes)
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x=F.relu(self.fc3(x))
-        x=F.relu(self.fc4(x))
-        x = self.fc5(x)
-        return F.softmax(x, dim=1)
-
 num_nodes=17
 num_edges=num_nodes*(num_nodes-1)//2
 input_dim=2
@@ -46,11 +30,9 @@ clique_r=4
 clique_s=4
 batch_size=256
 
-model = Net(input_dim, hidden_dim, num_classes)
+x=torch.randn(num_edges,input_dim, requires_grad=True)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-x=torch.randn(num_edges,input_dim)
+optimizer= torch.optim.Adam([x], lr=0.001)
 
 
 
@@ -59,15 +41,15 @@ def loss_func(probs, cliques_r, cliques_s,num_classes):
     edge_list = torch.combinations(torch.arange(num_nodes), 2)
     #dict to map edge to index
     edge_dict = {tuple(edge_list[i].tolist()): i for i in range(edge_list.size(0))} 
-    edge_dict.update({(edge[1], edge[0]): i for edge, i in edge_dict.items()})
+    edge_dict.update({(edge[1], edge[0]): i for edge, i in edge_dict.items()}) #add reverse edges
     
     for clique in cliques_r:
         #probabilities for an edge being blue
         probs_blue=probs[:,1]
         #get all edges in the clique
         clique_edge=torch.combinations(clique, r=2).t()
-        edge_indices = [edge_dict[tuple(edge.tolist())] for edge in clique_edge.t()]
-        edge_probs = probs_blue[edge_indices]
+        edge_indices = [edge_dict[tuple(edge.tolist())] for edge in clique_edge.t()] #get indices of edges in the clique
+        edge_probs = probs_blue[edge_indices] #get probabilities of edges in the clique
         blue_prod = edge_probs.prod()
         loss += blue_prod
         
@@ -85,8 +67,7 @@ def loss_func(probs, cliques_r, cliques_s,num_classes):
 
 
 
-def train_model(model,x, optimizer, clique_r, clique_s,batch_size,num_nodes):
-    model.train()
+def train_model(x, optimizer, clique_r, clique_s,batch_size,num_nodes):
     num_epochs = 1000
     all_cliques_r=torch.combinations(torch.arange(num_nodes),clique_r)
     all_cliques_s=torch.combinations(torch.arange(num_nodes),clique_s)
@@ -99,8 +80,7 @@ def train_model(model,x, optimizer, clique_r, clique_s,batch_size,num_nodes):
         cliques_s = random.sample(all_cliques_s.tolist(),batch_size)
         cliques_s=torch.tensor(cliques_s,dtype=torch.long)
     
-        probs = model(x)
-        
+        probs=F.softmax(x, dim=1)
         loss = loss_func(probs, cliques_r, cliques_s,num_classes)
         loss.backward()
         optimizer.step()
@@ -108,9 +88,7 @@ def train_model(model,x, optimizer, clique_r, clique_s,batch_size,num_nodes):
         if epoch % 10 == 0:  # Print loss every 10 epochs
             print(f'Epoch: {epoch}, Loss: {loss.item()}')
     
-    # Save the model after training
-    torch.save(model.state_dict(), 'model.pth')
-    
+    torch.save(x, 'edges.pth')
     return probs
 
 def cost_func(probs, cliques_r, cliques_s):
@@ -143,20 +121,19 @@ def cost_func(probs, cliques_r, cliques_s):
     
 #train_model(model, optimizer, clique_r, clique_s, batch_size,num_nodes)    
     
-def evaluate(model, x, clique_r, clique_s):
-    model.load_state_dict(torch.load('model.pth'))
-    model.eval()
+def evaluate(x, clique_r, clique_s):
     with torch.no_grad():
-        probs = model(x)
+        x=torch.load('edges.pth')
+        probs=F.softmax(x, dim=1)
         cliques_r=torch.combinations(torch.arange(num_nodes),clique_r)
         cliques_s=torch.combinations(torch.arange(num_nodes),clique_s)
         cost=cost_func(probs, cliques_r, cliques_s)
     return cost
 
 def main():
-    probs=train_model(model, x, optimizer,  clique_r, clique_s, batch_size,num_nodes)
+    probs=train_model(x, optimizer,  clique_r, clique_s, batch_size,num_nodes)
     print(probs, 'probs')
-    cost , sets= evaluate(model, x, clique_r, clique_s)
+    cost , sets= evaluate(x, clique_r, clique_s)
     print(f'Final cost: {cost}')
     return sets
 main() 
