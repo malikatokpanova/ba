f0ab30528d40de39d320cac4f4cda86345c0ac09
Malika Tokpanova <malikatokpanova@gmail.com>
2024-10-11 16:24:17 +0200

    gnn

--- new_model.py
+++ new_model.py
@@ -69,12 +69,12 @@ class ramsey_MPNN(torch.nn.Module):
         
         xinit=x.clone()
          
-        """ x=F.leaky_relu(self.conv1(x, edge_index))
+        x=F.leaky_relu(self.conv1(x, edge_index))
         x=F.dropout(x, p=self.dropout, training=self.training) 
         for conv in self.convs:
             x = F.leaky_relu(conv(x, edge_index))
             x = F.dropout(x, p=self.dropout, training=self.training) 
-        """
+        
         
         """ x=F.leaky_relu(self.lin1(x))
         x=F.dropout(x, p=self.dropout, training=self.training) 
@@ -108,7 +108,7 @@ class EdgePredNet(torch.nn.Module):
         super(EdgePredNet, self).__init__() 
         #self.lin = Sequential(Linear(2*num_features, hidden_channels), ReLU(), Linear(hidden_channels, 1),torch.nn.Sigmoid())
         self.dropout=dropout
-        self.lin1=Linear(num_features,hidden_channels)
+        self.lin1=Linear(hidden_channels,hidden_channels) # if no GNN, then Linerar(num_features, hidden_channels)
         #self.lin2=Linear(hidden_channels,hidden_channels)
         #self.lin3=Linear(hidden_channels,hidden_channels)
         self.lin4=Linear(hidden_channels,num_features)
