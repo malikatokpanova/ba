edfd5bb951d7c6ba7e2244d939471f17d03bd40c
Malika Tokpanova <malikatokpanova@gmail.com>
2024-10-18 18:35:30 +0200

    gcn

--- new_model.py
+++ new_model.py
@@ -78,7 +78,7 @@ class ramsey_MPNN(torch.nn.Module):
         
         xinit=x.clone()
          
-        x=F.leaky_relu(self.conv1(x, edge_index))
+        x=F.relu(self.conv1(x, edge_index))
         x=F.dropout(x, p=self.dropout, training=self.training) 
         for conv in self.convs[:-1]:
             x = F.relu(conv(x, edge_index))
