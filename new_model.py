5c927686b6be1109bcacada869c44327c25af8d2
Malika Tokpanova <malikatokpanova@gmail.com>
2024-10-18 13:46:26 +0200

    edgeprednet

--- new_model.py
+++ new_model.py
@@ -88,14 +88,14 @@ class ramsey_MPNN(torch.nn.Module):
         x = self.convs[-1](x, edge_index)  """
         
         
-        x=F.leaky_relu(self.lin1(x))
+        """ x=F.leaky_relu(self.lin1(x))
         x=F.dropout(x, p=self.dropout, training=self.training) 
         x=F.leaky_relu(self.lin2(x)) 
         x=F.dropout(x, p=self.dropout, training=self.training)
         #x=F.leaky_relu(self.lin3(x))
         #x=F.dropout(x, p=self.dropout, training=self.training)
         x=self.lin4(x)
-        x=x+xinit  #skip connection 
+        x=x+xinit  #skip connection  """
                   
 
         """ x_i = x[edge_index[0], :]
