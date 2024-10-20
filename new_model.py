ef05c6d1e8452ddf8d7459d1911db35a0065b991
Malika Tokpanova <malikatokpanova@gmail.com>
2024-10-20 14:04:01 +0200

    mlp

--- new_model.py
+++ new_model.py
@@ -84,14 +84,14 @@ class ramsey_MPNN(torch.nn.Module):
 
         
         
-        """ x=F.leaky_relu(self.lin1(x))
+        x=F.leaky_relu(self.lin1(x))
         x=F.dropout(x, p=self.dropout, training=self.training) 
         #x=F.leaky_relu(self.lin2(x)) 
         #x=F.dropout(x, p=self.dropout, training=self.training)
         #x=F.leaky_relu(self.lin3(x))
         #x=F.dropout(x, p=self.dropout, training=self.training)
         x=self.lin4(x)
-        x=x+xinit  #skip connection   """
+        x=x+xinit  #skip connection   
                   
 
         """ x_i = x[edge_index[0], :]
@@ -121,7 +121,7 @@ class EdgePredNet(torch.nn.Module):
         self.lin2=Linear(hidden_channels,hidden_channels)
         self.lin3=Linear(hidden_channels,hidden_channels)
         self.lin4=Linear(hidden_channels,num_features) """
-        self.lin5 = Linear(hidden_channels, hidden_channels)
+        self.lin5 = Linear(2*num_features, hidden_channels)
         self.lin6 = Linear(hidden_channels, num_classes)
     def forward(self, x, edge_index,xinit):
         #x=F.leaky_relu(self.lin1(x))
@@ -135,9 +135,9 @@ class EdgePredNet(torch.nn.Module):
         
         x_i = x[edge_index[0], :] #edge_index[0] contains the source nodes
         x_j = x[edge_index[1], :] #edge_index[1] contains the target nodes
-        #edge_features = torch.cat([x_i, x_j], dim=-1)  
-        #edge_pred= F.relu(self.lin5(edge_features))
-        edge_pred= F.relu(self.lin5(x_i * x_j))
+        edge_features = torch.cat([x_i, x_j], dim=-1)  
+        edge_pred= F.relu(self.lin5(edge_features))
+        #edge_pred= F.relu(self.lin5(x_i * x_j))
         #edge_pred = F.relu(self.lin5(torch.sum(x_i * x_j, dim=-1, keepdim=True)))
         #edge_pred = F.dropout(edge_pred, p=self.dropout, training=self.training)
         edge_pred = self.lin6(edge_pred)
