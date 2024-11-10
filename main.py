e464b0ccba8c42157ad7f1781e2e273e5fb3ad08
Malika Tokpanova <malikatokpanova@gmail.com>
2024-11-10 14:04:57 +0100

    EdgeNet

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from torch.nn import Linear, Sequential, ReLU, BatchNorm1d as BN
 
 import random
 from random import sample
-from new_model import ramsey_NN, loss_func, cost
+from edgenet import ramsey_NN, loss_func, cost
 
 import wandb
 
