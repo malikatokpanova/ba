01b62929fa4b7e6b55f099cbaa6d9c407ccd1157
Malika Tokpanova <malikatokpanova@gmail.com>
2024-11-22 20:04:45 +0100

    R(3,4)

--- mpnn_run.py
+++ mpnn_run.py
@@ -34,14 +34,14 @@ config=dict(
         num_layers=5,
         dropout=0.1,
         num_cliques=128,
-        epochs=100,
+        epochs=500,
         conv_type='GIN',
 )
 
 graph_parameters={
-    'num_nodes': 5,   
+    'num_nodes': 8,   
     'clique_r':3,
-    'clique_s':3,
+    'clique_s':4,
     'num_classes':2
 }
 
