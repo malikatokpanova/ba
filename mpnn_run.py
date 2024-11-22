19371cbb02b220837f1e370a99cbb4187f649f10
Malika Tokpanova <malikatokpanova@gmail.com>
2024-11-22 16:19:42 +0100

    R(3,4)

--- mpnn_run.py
+++ mpnn_run.py
@@ -34,12 +34,12 @@ config=dict(
         num_layers=5,
         dropout=0.1,
         num_cliques=128,
-        epochs=10000,
+        epochs=500,
 )
 
 graph_parameters={
-    'num_nodes': 17,   
-    'clique_r':4,
+    'num_nodes': 8,   
+    'clique_r':3,
     'clique_s':4,
     'num_classes':2
 }
