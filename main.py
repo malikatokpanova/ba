07779749334e5649b6bc65aef2adf3dcaa62de19
Malika Tokpanova <malikatokpanova@gmail.com>
2024-11-24 17:29:45 +0100

    R(3,4)

--- main.py
+++ main.py
@@ -38,13 +38,13 @@ config=dict(
         num_layers=5,
         dropout=0.3,
         num_cliques=128,
-        epochs=100,
+        epochs=500,
 )
 
 graph_parameters={
-    'num_nodes': 5,   
+    'num_nodes': 8,   
     'clique_r':3,
-    'clique_s':3,
+    'clique_s':4,
     'num_classes':2
 }
 
